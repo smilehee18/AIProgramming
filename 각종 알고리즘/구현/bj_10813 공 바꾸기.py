c794b4661533cf4109c43ba91e3656c7dd5bb188
Sohee Park <123307856+smilehee18@users.noreply.github.com>
2024-01-04 00:08:56 +0900

    Update bj_10813 공 바꾸기.py

--- 각종 알고리즘/구현/bj_10813 공 바꾸기.py
+++ 각종 알고리즘/구현/bj_10813 공 바꾸기.py
@@ -1,3 +1,8 @@
+'''
+[놓친 부분] 
+map(str, bag) -> bag 리스트를 묶어서 문자열로 변환해준다.
+.join() 함수 익히기 -> ' '공백을 문자열 사이사이에 넣어줄 때 유용함 
+'''
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
@@ -8,4 +13,4 @@ for i in range(m):
     bag[i-1], bag[j-1] = bag[j-1], bag[i-1]
     #print(bag)
 
-print(' '.join(map(str, bag)))
+print(' '.join(map(str, bag))) 
